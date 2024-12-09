fe23e472cd631641211f64487a8b9764521b7e9e
sg788 <sumangtm1083@gmail.com>
2024-12-10 01:15:04 +0630

    Feat: Update AdvanceDotNetBatch1.Shared

---
+++ AdvanceDotNetBatch1.Shared/Entension.cs
@@ -0,0 +1,14 @@
+﻿namespace AdvanceDotNetBatch1.Shared
+{
+    public static class Entension
+    {
+        public static IQueryable<TSource> Paginate<TSource>(
+        this IQueryable<TSource> source,
+        int pageNo,
+        int pageSize
+    )
+        {
+            return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
