0c7212df4874a2f53360b2e03b269a7a5cef3f1f
sg788 <sumangtm1083@gmail.com>
2024-12-11 14:47:09 +0630

    Serilog

---
+++ SMDotNet.Serilog/Controllers/LogController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace SMDotNet.Serilog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet] //methods are just API endpoint actions and don't specifically involve CRUD.
+        public IActionResult Test() 
+        {
+            _logger.LogInformation("Hello!");
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            _logger.LogWarning("This is a warning log.");
+            return StatusCode(201);
+        }
+    }
+}
---
+++ SMDotNet.Serilog/Program.cs
@@ -0,0 +1,45 @@
+using Serilog.Formatting.Compact;
+using Serilog.Templates;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File(
+        new CompactJsonFormatter(),
+        "logs/RestApi.txt",
+        rollingInterval: RollingInterval.Day
+    )
+    .WriteTo.Console(new ExpressionTemplate("[{@t:HH:mm:ss} {@l:u3} {SourceContext}] {@m}\n{@x}"))
+    //      .WriteTo
+    //.MSSqlServer(
+    //    connectionString: builder.Configuration.GetConnectionString("DbConnection"),
+    //    sinkOptions: new MSSqlServerSinkOptions { TableName = "Tbl_Logs", AutoCreateSqlTable = true })
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
