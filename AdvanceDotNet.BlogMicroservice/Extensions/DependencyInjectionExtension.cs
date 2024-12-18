d729502ca3272d90ea0b251ae8fd9e128d5e80e9
sg788 <sumangtm1083@gmail.com>
2024-12-18 15:45:50 +0630

    -

--- AdvanceDotNet.BlogMicroservice/Extensions/DependencyInjectionExtension.cs
+++ AdvanceDotNet.BlogMicroservice/Extensions/DependencyInjectionExtension.cs
@@ -1,6 +1,36 @@
-﻿namespace AdvanceDotNet.BlogMicroservice.Extensions
+﻿using AdvanceDotNet.BlogMicroservice.Features;
+using AdvanceDotNetBatch1.Database.Models;
+using AdvanceDotNetBatch1.RepositoryPattern.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvanceDotNet.BlogMicroservice.Extensions;
+
+public static class DependencyInjectionExtension
 {
-    public class DependencyInjectionExtension
+    public static IServiceCollection AddDependencies(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
+        builder.Services.AddMediatR(opt =>
+        {
+            opt.RegisterServicesFromAssembly(typeof(DependencyInjectionExtension).Assembly);
+        });
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        builder
+            .Services.AddControllers()
+            .AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
+
+        return services;
     }
+
 }
