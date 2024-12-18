6cd7776443c7c6d969b41621e6b5132fb4b0de9f
sg788 <sumangtm1083@gmail.com>
2024-12-18 12:21:18 +0630

    -

--- AdvanceDotNet.BlogMicroservice/Features/IRepositoryBase.cs
+++ AdvanceDotNet.BlogMicroservice/Features/IRepositoryBase.cs
@@ -1,6 +1,24 @@
-﻿namespace AdvanceDotNet.BlogMicroservice.Features
+﻿using System.Linq.Expressions;
+
+namespace AdvanceDotNet.BlogMicroservice.Features
 {
-    public interface IRepositoryBase
+    public interface IRepositoryBase<T> where T : class
+
     {
+        IQueryable<T> Query(Expression<Func<T, bool>> expression);
+
+        IEnumerable<T>GetAll();
+        Task AddAsync(T entity, CancellationToken cs= default);
+        void Add(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cs= default);
+        void AddRange(T entity);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+        void SaveChanges();
+        void SaveChangesAsync(CancellationToken cancellationToken);
+
+
     }
 }
