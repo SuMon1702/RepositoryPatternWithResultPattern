46465fd653c45e32e2cd23f9d1a364ca89fc87bb
sg788 <sumangtm1083@gmail.com>
2024-12-18 13:05:13 +0630

    -

--- AdvanceDotNet.BlogMicroservice/Features/IRepositoryBase.cs
+++ AdvanceDotNet.BlogMicroservice/Features/IRepositoryBase.cs
@@ -11,13 +11,13 @@ namespace AdvanceDotNet.BlogMicroservice.Features
         Task AddAsync(T entity, CancellationToken cs= default);
         void Add(T entity);
         Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cs= default);
-        void AddRange(T entity);
+        void AddRange(IEnumerable<T> entities);
         void Update(T entity);
         void UpdateRange(IEnumerable<T> entities);
         void Delete(T entity);
         void DeleteRange(IEnumerable<T> entities);
         void SaveChanges();
-        Task SaveChangesAsync(CancellationToken cancellationToken);
+        Task SaveChangesAsync(CancellationToken  cs= default);
 
 
     }
