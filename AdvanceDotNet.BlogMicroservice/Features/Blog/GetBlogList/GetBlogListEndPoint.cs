206e700b2ac6523053de2d5ab7a6e422d83b9ca6
sg788 <sumangtm1083@gmail.com>
2024-12-18 11:46:16 +0630

    -

---
+++ AdvanceDotNet.BlogMicroservice/Features/Blog/GetBlogList/GetBlogListEndPoint.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceDotNet.BlogMicroservice.Features.Blog.GetBlogList
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetBlogListEndPoint : ControllerBase
+    {
+    }
+}
